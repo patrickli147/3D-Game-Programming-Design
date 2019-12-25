113b8cebdf44a2059ff542007f278d5217d394a4
patrickli147 <49908827+patrickli147@users.noreply.github.com>
2019-12-25 18:19:04 +0800

    hw11

---
+++ Homework11/New Unity Project/Assets/VRbutton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+ 
+public class VRbutton : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject plane;
+    public GameObject VRbutton_up;
+    public GameObject VRbutton_down;
+    public VirtualButtonBehaviour[] VRbehaviours;
+
+    void Start()
+    {
+        VRbehaviours = GetComponentsInChildren<VirtualButtonBehaviour>();
+        for (int i = 0; i < VRbehaviours.Length; i++)
+        {
+            VRbehaviours[i].RegisterEventHandler(this);
+        }
+        plane = GameObject.Find("tree");
+        VRbutton_up = GameObject.Find("up");
+        VRbutton_down = GameObject.Find("down");
+        Debug.Log("ready\n");
+    }
+
+    void Update()
+    {
+
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour myButton)
+    {        
+        plane.transform.position += new Vector3(0, 0.03f, 0);
+    }
+    
+    public void OnButtonReleased(VirtualButtonBehaviour myButton)
+    {
+
+    }
+}
